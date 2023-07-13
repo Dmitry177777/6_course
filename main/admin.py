dd081a91bdff037b36e1eaf809e942fc72bef396
Dmitry <k1791@mail.ru>
2023-07-13 19:42:45 +0500

    start4

--- main/admin.py
+++ main/admin.py
@@ -1,33 +1,42 @@
 from django.contrib import admin
 
-from main.models import Product, Category, Blog, Version
+from main.models import Client, MailingSetting, Blog, Message, MailingLogs
 
 
 # Register your models here.
 # admin.site.register(Product)
 # admin.site.register(Category)
 
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('product_name', 'description','product_category',  )
-    search_fields = ('product_name', 'description',)
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('email', 'client','client_comment',  )
+    search_fields = ('email', 'client',)
     list_filter = ('is_active', )
 
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('product_category', 'description', )
-    search_fields = ('product_category', 'description',)
-    list_filter = ('is_active', )
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('email', 'head_message','body_message', )
+    search_fields = ('email', 'head_message','body_message',)
+    list_filter = ('head_message', )
+
+@admin.register(MailingLogs)
+class MailingLogsAdmin(admin.ModelAdmin):
+    list_display = ('email', 'head_message','body_message', )
+    search_fields = ('email', 'head_message','body_message',)
+    list_filter = ('head_message', )
+
+
+
+@admin.register(MailingSetting)
+class MailingSettingAdmin(admin.ModelAdmin):
+    list_display = ('email', 'log_time','status_mailing', 'get_server_mail', )
+    search_fields = ('email', 'log_time','status_mailing',)
+    list_filter = ('get_server_mail', )
 
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ('message_heading', 'message_content','message_preview',  )
-    search_fields = ('message_heading', 'message_content',)
+    list_display = ('message_preview', 'message_heading','message_content', 'date_of_creation', 'date_of_change', )
+    search_fields = ('message_preview', 'message_heading', 'message_content', 'date_of_creation', 'date_of_change',)
     list_filter = ('is_publication', )
     prepopulated_fields = {"slug": ("message_heading",)}
 
-@admin.register(Version)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('product_name', 'product_version','description', )
-    search_fields = ('product_name', 'description',)
-    list_filter = ('is_active', )
